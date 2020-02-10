c696fc3767fff2c5c9057e74f707533679d8cb30
Jeremias Wiedmann <wiedmann@cqse.eu>
2020-02-10 18:14:57 +0100

    Fix javadoc.

--- src/main/java/eu/cqse/teamscale/jenkins/upload/TeamscaleUploadUtilities.java
+++ src/main/java/eu/cqse/teamscale/jenkins/upload/TeamscaleUploadUtilities.java
@@ -16,7 +16,7 @@ public class TeamscaleUploadUtilities {
      * Find files in a directory with ant pattern.
      * @param dirToScan top-level directory to start scanning.
      * @param pattern ant pattern to look for files.
-     * @return found files with {@param pattern} or empty
+     * @return found files with {@code pattern} or {@code empty}
      * @throws IOException scanning directory.
      */
     public static List<File> getFiles(File dirToScan, String pattern) throws IOException {
